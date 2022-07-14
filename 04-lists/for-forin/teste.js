7c47559325bdefef3bd5f43b569fcc188648ae13
lipefreire <felipef12123@gmail.com>
2022-07-14 08:54:06 -0300

    Criando nosso próprio Array.Reduce

--- 04-lists/for-forin/teste.js
+++ 04-lists/for-forin/teste.js
@@ -1,24 +1,25 @@
-const { obterPessoas } = require('./service')
+const {
+    obterPessoas
+} = require('./service')
 
 /*
 
  const item = {
      nome: 'Erick',
-     idade: 12
+     idade: 12,
 
-}
+ }
 
-const { nome, idade } = item
-console.log(nome, idade)
-
+ const { nome , idade } = item
+ console.log(nome, idade)
 */
 Array.prototype.meuFilter = function (callback) {
     const lista = []
-    for(index in this) {
+    for (index in this) {
         const item = this[index]
         const result = callback(item, index, this)
-        // 0, "", null ou undefined === false
-        if(!result) continue;
+        // 0, "", null, undefined === false
+        if (!result) continue;
         lista.push(item)
     }
     return lista;
@@ -26,15 +27,17 @@ Array.prototype.meuFilter = function (callback) {
 
 async function main() {
     try {
-        const { results } = await obterPessoas(`a`)
+        const {
+            results
+        } = await obterPessoas(`a`)
 
-        // const familiaLars = results.filter(function(item) {
-        //                 // POR PADRÃO PRECISA RETORNAR UM BOOLEANO
-        //                 //PARA INFORMAR SE DEVE MANTER OU REMOVER DA LISTA
-        //                 // false > remove da lista
-        //                 // true > mantem na lista
-        //                 // Não encontrou = -1
-        //                 // Se encontrou = posicaoNoArray
+        // const familiaLars = results.filter(function (item) {
+        //     // por padrão precisa retornar um booleano
+        //     // para informar se deve manter ou remover da lista
+        //     // false > remove da lista
+        //     // true > mantem
+        //     // não encontrou = -1
+        //     // encontrou = posicaoNoArray
         //     const result = item.name.toLowerCase().indexOf(`lars`) !== -1
         //     return result
         // })
@@ -42,12 +45,13 @@ async function main() {
             console.log(`index: ${index}`, lista.length)
             return item.name.toLowerCase().indexOf('lars') !== -1
         })
+
         const names = familiaLars.map((pessoa) => pessoa.name)
         console.log(names)
-    }
-    catch(error) {
+
+    } catch (error) {
         console.error('DEU RUIM', error)
     }
-}
 
+}
 main()
